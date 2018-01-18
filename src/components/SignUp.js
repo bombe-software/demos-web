b9240ada0b3d01b8be915c833c4557ff4d3db681
HGWells07 <carlitose07@gmail.com>
2018-01-17 19:24:58 -0600

    Agregado state y submitHandle signUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            nombre: 'Nombre',
+            email: 'email',
+            password: 'password',
+            curp: 'curp',
+            avatar: 'avatar',
+            localidad: 'localidad'
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        const {
+            nombre, email, password,
+            curp, avatar, localidad
+        } = this.state
+        this.props.mutate({
+            variables: {
+                nombre, email, password,
+                curp, avatar, localidad
+            }
+        }).then(Alert.alert('Informacion enviada'));
+    }
+
     render() {
         return (
             <div className="is-title">Sign Up</div>
