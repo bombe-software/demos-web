e96ed3486d5bd16c70edfc2a5b755dd803983b06
MedinaVilla <diletalvezalaautopista@gmail.com>
2018-03-19 11:34:29 -0600

    Arreglar bugs 2

--- src/components/moderador/Politico/solicitud_politico_modif.js
+++ src/components/moderador/Politico/solicitud_politico_modif.js
@@ -118,7 +118,7 @@ export default compose(
     graphql(fetchSolicitudPoliticoModif, {
         name: 'fetchSolicitudPoliticoModif'
     }),
-    graphql(AceptarModificarSolicitudPolitico, {
+    graphql(AceptarPolitico, {
       name: 'AceptarModificarSolicitudPolitico'
     }),
     graphql(DenegarPolitico, {
